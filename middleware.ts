4d9fb66edef0d3f7001b010d22b9f3c1362c80cf
Haniel Ryan <hanielryanephin@gmail.com>
2024-05-23 20:05:02 +0400

    last

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,20 @@
-export {default} from "next-auth/middleware";
-export const config = {matcher:["/home/","/home/done","/home/pending"]}
+import { NextRequest, NextResponse } from 'next/server';
+import nextAuthMiddleware, { NextRequestWithAuth } from 'next-auth/middleware';
+
+export async function middleware(req: NextRequest) {
+  const emailCookie = req.cookies.get('email');
+
+  if (emailCookie) {
+    return NextResponse.redirect(new URL('/otp', req.nextUrl.origin));
+  }
+
+  // Typecast req to NextRequestWithAuth before passing it to nextAuthMiddleware
+  return nextAuthMiddleware(req as NextRequestWithAuth);
+}
+
+export const config = {
+  matcher: ['/home/', '/home/done', '/home/pending'],
+};
+
+// Make sure to export default as the custom middleware function
+export { middleware as default };
