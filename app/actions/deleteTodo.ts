62c6453ac1d68ac893597bea0ef019a89d256f0b
Haniel Ryan <143314778+hanielRyan@users.noreply.github.com>
2024-06-29 20:26:03 +0400

    Update deleteTodo.ts

--- app/actions/deleteTodo.ts
+++ app/actions/deleteTodo.ts
@@ -2,6 +2,6 @@
 import { revalidatePath } from "next/cache";
 import axios from "axios";
 export const deleteTodo = async(_id:string) => {
-    await axios.delete(`https://todos-task-manager-back.onrender.com/post/deletePost/${_id}`);
+    await axios.delete(`https://full-stack7back.vercel.app/post/deletePost/${_id}`);
     revalidatePath("/home");
 }
