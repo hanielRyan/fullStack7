6923c340f0dd35e3e336a5ce4c026d866e0e19af
Haniel Ryan <143314778+hanielRyan@users.noreply.github.com>
2024-06-29 20:26:44 +0400

    Update uploadPic.ts

--- app/actions/uploadPic.ts
+++ app/actions/uploadPic.ts
@@ -2,6 +2,6 @@
 import axios from "axios";
 import { revalidatePath } from "next/cache";
 export const uploadPic = async(formData:FormData)=>{
-await axios.post("https://todos-task-manager-back.onrender.com/uploadPic",formData);
+await axios.post("https://full-stack7back.vercel.app/uploadPic",formData);
 revalidatePath("/home");
 }
